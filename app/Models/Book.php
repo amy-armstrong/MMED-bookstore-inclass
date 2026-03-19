86f9d9a5ede05d3851b978de0267ca8eeffe7900
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-03-18 22:05:56 -0400

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable =  [
         'title'
